47b64de2e32280343c1899f164bf38b10be8bcbc
Serenacula <faylee@hanginggardens.com>
2022-01-04 05:55:42 +0000

    added footer, refactored some css

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import {combineClasses as combine} from "../resources/scripts/utilities"
 // NEXT Components
 import Head from "next/head"
 import Image from "next/image"
+import Link from "next/link"
 
 // CSS
 import styles from "../css/index.module.scss"
@@ -42,13 +43,12 @@ export default function Home() {
                     </div>
                 </div>
                 
-                {/* second section removed */}
 			
-                <div className={styles.thirdSection}>
+                <div className={styles.secondSection}>
                 
                     <h2 className={styles.skillsTitle}>Skills</h2>
                     
-                    <div className={styles.thirdSectionMain}>
+                    <div className={styles.secondSectionMain}>
                         
                         <h2 className={styles.sectionTitle}>Starting with the fundamentals</h2>
                         
@@ -69,7 +69,7 @@ export default function Home() {
                     </div>
                 </div>
 			
-                <div className={styles.fourthSection}>
+                <div className={styles.thirdSection}>
                         
                     <h2 className={styles.sectionTitle}>Learning the backend</h2>
                     
@@ -88,22 +88,30 @@ export default function Home() {
                     </div>
                 </div>
 			
-                <div className={styles.fifthSection}>
+                <div className={styles.fourthSection}>
                         
-                        <h2 className={styles.sectionTitle}>Building with modern frameworks</h2>
+                    <h2 className={styles.sectionTitle}>Building with modern frameworks</h2>
+                    
+                    <div className={styles.card}>
                         
-                        <div className={styles.card}>
-                            
-                            <h3>React</h3>
-                            <p>My framework of choice, as well as an industry standard, I have spent the last few months months getting comfortable with React. I find it both intuitive and well thought-out, and it is a skillset I wish to truly master.</p>
-                            
-                            <h3>Next.js</h3>
-                            <p>The framework that made this site! Next.js builds on top of React to create a complete design framework. It takes care of all the backend and under-the-hood bits, making it ideal for frontend developers.</p>
-                            
-                            <h3>GSAP</h3>
-                            <p>A modern animation framework that works well with React, GSAP is my current choice for making beautiful and easy animations.</p>
-                        </div>
+                        <h3>React</h3>
+                        <p>My framework of choice, as well as an industry standard, I have spent the last few months months getting comfortable with React. I find it both intuitive and well thought-out, and it is a skillset I wish to truly master.</p>
+                        
+                        <h3>Next.js</h3>
+                        <p>The framework that made this site! Next.js builds on top of React to create a complete design framework. It takes care of all the backend and under-the-hood bits, making it ideal for frontend developers.</p>
+                        
+                        <h3>GSAP</h3>
+                        <p>A modern animation framework that works well with React, GSAP is my current choice for making beautiful and easy animations.</p>
                     </div>
+                </div>
+                
+                <div className={styles.fifthSection}>
+                    <Link href="/portfolio">
+                        <a>
+                        <h2 className={styles.footerTitle}>Visit Portfolio »</h2>
+                        </a>
+                    </Link>
+                </div>
             </main>
 		</div>
 	)
